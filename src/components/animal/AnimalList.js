2260faad1cf9166bb1022b2e3af8103f0d64027f
bpotts11 <brittneympotts@gmail.com>
2021-03-02 13:35:40 -0600

    created forms that allowed adding new animals, employees, and locations

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from "react"
+import { useHistory } from "react-router-dom"
 import { AnimalContext } from "./AnimalProvider"
 import { LocationContext } from "../location/LocationProvider"
 import { CustomerContext } from "../customer/CustomerProvider"
@@ -11,30 +12,32 @@ export const AnimalList = () => {
     const { locations, getLocations } = useContext(LocationContext)
     const { customers, getCustomers } = useContext(CustomerContext)
 
+    // The useHistory hook let's us tell React which route we want to visit. We will use it to tell React to render the animal form component.
+    const history = useHistory()
+
     //useEffect - reach out to the world for something
     useEffect(() => {
         // console.log("AnimalList: useEffect - getAnimals")
         getLocations()
             .then(getCustomers)
             .then(getAnimals)
-
     }, [])
 
 
     return (
         <>
-            <h4>Animals</h4>
+            <h2>Animals</h2>
+            <button onClick={() => { history.push("/animals/create") }}>Add Animal</button>
             <div className="animals">
                 {animals.map(animal => {
                     const owner = customers.find(c => c.id === animal.customerId)
-                    const clinic = locations.find(l => l.id === animal.locationId)
+                    const location = locations.find(l => l.id === animal.locationId)
 
                     return <AnimalCard key={animal.id}
-                        location={clinic}
+                        location={location}
                         customer={owner}
                         animal={animal} />
-                })
-                }
+                })}
             </div>
         </>
     )
--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,28 +1,39 @@
 import React, { useContext, useEffect } from "react"
+import { useHistory } from "react-router-dom"
 import { EmployeeContext } from "./EmployeeProvider"
+import { LocationContext } from "../location/LocationProvider"
 import { EmployeeCard } from "./EmployeeCard"
 import "./Employee.css"
 
 export const EmployeeList = () => {
     // This state changes when `getEmployees()` is invoked below
     const { employees, getEmployees } = useContext(EmployeeContext)
+    const { locations, getLocations } = useContext(LocationContext)
+
+    // The useHistory hook let's us tell React which route we want to visit. We will use it to tell React to render the animal form component.
+    const history = useHistory()
 
     //useEffect - reach out to the world for something
     useEffect(() => {
         // console.log("EmployeeList: useEffect - getEmployees")
-        getEmployees()
-
+        getLocations()
+            .then(getEmployees)
     }, [])
 
 
     return (
-        <div className="employees">
-            {/* {console.log("EmployeeList: Render", employees)} */}
-            {
-                employees.map(employee => {
-                    return <EmployeeCard key={employee.id} employee={employee} />
-                })
-            }
-        </div>
+        <>
+            <h2>Employees</h2>
+            <button onClick={() => { history.push("/employees/create") }}>New Employee</button>
+            <div className="employees">
+                {employees.map(employee => {
+                    const location = locations.find(l => l.id === employee.locationId)
+
+                    return <EmployeeCard key={employee.id}
+                        location={location}
+                        employee={employee} />
+                })}
+            </div>
+        </>
     )
 }
