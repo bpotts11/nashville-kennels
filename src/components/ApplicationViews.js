75a7e8bc1f78fda3b4a9feaae367f07c04e68eb9
bpotts11 <brittneympotts@gmail.com>
2021-03-01 23:30:57 -0600

    fixed the animal card component ot use api

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,12 +3,12 @@ import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalList } from "./animal/AnimalList"
 import { AnimalProvider } from "./animal/AnimalProvider"
-import { CustomerList } from "./customer/CustomerList";
-import { CustomerProvider } from "./customer/CustomerProvider";
-import { EmployeeList } from "./employee/EmployeeList";
-import { EmployeeProvider } from "./employee/EmployeeProvider";
-import { LocationList } from "./location/LocationList";
-import { LocationProvider } from "./location/LocationProvider";
+// import { CustomerList } from "./customer/CustomerList";
+// import { CustomerProvider } from "./customer/CustomerProvider";
+// import { EmployeeList } from "./employee/EmployeeList";
+// import { EmployeeProvider } from "./employee/EmployeeProvider";
+// import { LocationList } from "./location/LocationList";
+// import { LocationProvider } from "./location/LocationProvider";
 
 export const ApplicationViews = () => {
     return (
@@ -19,29 +19,29 @@ export const ApplicationViews = () => {
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalProvider>
+            <AnimalProvider>
+                <Route exact path="/animals">
                     <AnimalList />
-                </AnimalProvider>
-            </Route>
+                </Route>
+            </AnimalProvider>
             {/* Render the animal list when http://localhost:3000/customers */}
-            <Route path="/customers">
+            {/* <Route path="/customers">
                 <CustomerProvider>
                     <CustomerList />
                 </CustomerProvider>
-            </Route>
+            </Route> */}
             {/* Render the animal list when http://localhost:3000/employees */}
-            <Route path="/employees">
+            {/* <Route path="/employees">
                 <EmployeeProvider>
                     <EmployeeList />
                 </EmployeeProvider>
-            </Route>
+            </Route> */}
             {/* Render the animal list when http://localhost:3000/locations */}
-            <Route path="/locations">
+            {/* <Route path="/locations">
                 <LocationProvider>
                     <LocationList />
                 </LocationProvider>
-            </Route>
+            </Route> */}
         </>
     )
 }
